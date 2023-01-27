96619b03ff3365ee77e55e51b01932f9aa37e9bf
sam3797 <sampath16b@gmail.com>
2023-01-27 16:17:56 +0530

    Update provider.tf

--- terraform/eks/provider.tf
+++ terraform/eks/provider.tf
@@ -1,14 +1,16 @@
-#terraform {
-# required_providers {
-#  aws = {
-#   source = "hashicorp/aws"
-#  }
-
-provider "aws" {
-  region = "us-east-1"
+terraform {
+required_providers {
+ aws = {
+  source = "hashicorp/aws"
   access_key = "AKIAQTYCPYRQKA4JKASZ" 
   secret_key = "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"
-}
+ }
+
+# provider "aws" {
+#   region = "us-east-1"
+#   access_key = "AKIAQTYCPYRQKA4JKASZ" 
+#   secret_key = "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"
+# }
 
 kubernetes = {
       source  = "hashicorp/kubernetes"
