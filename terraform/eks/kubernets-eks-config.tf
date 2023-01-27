7d4082556c24c3e1f8c090c04fb4f8a273a1946f
sam3797 <sampath16b@gmail.com>
2023-01-27 16:09:37 +0530

    Add files via upload

--- terraform/eks/kubernets-eks-config.tf
+++ terraform/eks/kubernets-eks-config.tf
@@ -1,26 +1,26 @@
-# data "terraform_remote_state" "eks" {
-#   backend = "s3"
+data "terraform_remote_state" "eks" {
+  backend = "s3"
 
-#   config = {
-#       region = "us-east-1"
-#       bucket = "statefiles-terraform-github"
-#       key    = "eks.tfstate"
-#   }
-# }
+  config = {
+      region = "us-east-1"
+      bucket = "statefiles-terraform-github"
+      key    = "eks.tfstate"
+  }
+}
 
-# Retrieve EKS cluster information
-# provider "aws" {
-#   //region = data.terraform_remote_state.eks.outputs.region
-#   region = "us-east-1"
-# }
+#Retrieve EKS cluster information
+provider "aws" {
+  //region = data.terraform_remote_state.eks.outputs.region
+  region = "us-east-1"
+}
 
-# data "aws_eks_cluster" "cluster" {
-#   name = data.terraform_remote_state.eks.outputs.cluster_name
-# }
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
+}
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.my-eks-cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.my-eks-cluster.certificate_authority.0.data)
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
@@ -28,7 +28,7 @@ provider "kubernetes" {
       "eks",
       "get-token",
       "--cluster-name",
-      data.aws_eks_cluster.my-eks-cluster.name
+      data.aws_eks_cluster.cluster.name
     ]
   }
 }
